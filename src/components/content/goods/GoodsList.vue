db9798229060520dd8420ffe1803af61ffdc0d15
qjz <1518886943.com>
2021-07-23 14:46:46 +0800

    代码提交

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="good">
+      <good-item v-for="item in goods" :good="item"></good-item>
+  </div>
+</template>
+<script>
+import GoodItem from './GoodItem.vue';
+
+export default {
+  components: { GoodItem },
+  name: "GoodsList",
+  props:{
+      goods:{
+          type:Array,
+          default(){
+              return []
+          }
+      }
+  }
+};
+</script>
+<style>
+.good{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 5px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,8 +6,10 @@
     <feature-view></feature-view>
     <tab-control
       class="tbl-control"
+      @tabClick="tabClick"
       :titles="['流行', '新款', '精选']"
     ></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
     <ul>
       <li>123123123</li>
       <li>123123123</li>
@@ -73,14 +75,31 @@ import FeatureView from "./childComps/FeatureView.vue";
 
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl.vue";
+import GoodsList from "components/content/goods/GoodsList.vue";
 
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getGooddata } from "network/home";
+
 export default {
   name: "Home",
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: {
+          page: 0,
+          list: [],
+        },
+        new: {
+          page: 0,
+          list: [],
+        },
+        sell: {
+          page: 0,
+          list: [],
+        },
+      },
+      currentTagType: "pop",
     };
   },
   components: {
@@ -89,15 +108,56 @@ export default {
     RecommendView,
     FeatureView,
     TabControl,
+    GoodsList,
   },
   created() {
-    //函数函数一旦执行完毕 会被内存回收释放 局部变量
-    //函数调用->压入函数栈（保存函数调用过程中产生的所有变量）
-    //函数调用结束 -> 弹出函数变量（释放函数中所有变量）
-    getHomeMultidata().then((res) => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata();
+    this.getHomeGood("pop");
+    this.getHomeGood("new");
+    this.getHomeGood("sell");
+  },
+  methods: {
+    //流行tab切换
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentTagType = "pop";
+          break;
+        case 1:
+          this.currentTagType = "new";
+          break;
+        case 2:
+          this.currentTagType = "sell";
+          break;
+      }
+    },
+
+    //网络监听
+    getHomeMultidata() {
+      //函数函数一旦执行完毕 会被内存回收释放 局部变量
+      //函数调用->压入函数栈（保存函数调用过程中产生的所有变量）
+      //函数调用结束 -> 弹出函数变量（释放函数中所有变量）
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGood(type) {
+      const page = this.goods[type].page + 1;
+      getGooddata(type, page).then((res) => {
+        console.log(res);
+        if (res.data.list != null) {
+          //会将数组进行结构
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        }
+      });
+    },
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentTagType].list;
+    },
   },
 };
 </script>
